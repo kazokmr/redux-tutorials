57c6f27ef246b404598cd8cf6edb8e421145a540
Kazuhiko Okumura <engkokmr@gmail.com>
2021-11-17 06:11:10 +0900

    Redux Essentials Part4. Using Redux Data
    投稿日時を追加
    - postにDateを追加し記事を作成日時を登録できるようにした
    - 記事への表示は現在からどの位前に投稿されたかとしている
    - 記事リストは最新の投稿日時順で表示するようにした

--- redux-example-app/src/features/posts/SinglePostPage.js
+++ redux-example-app/src/features/posts/SinglePostPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import {useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 import {PostAuthor} from "./PostAuthor";
+import {TimeAgo} from "./TimeAgo";
 
 export const SinglePostPage = ({match}) => {
   const {postId} = match.params;
@@ -22,7 +23,10 @@ export const SinglePostPage = ({match}) => {
     <section>
       <article className="post">
         <h2>{post.title}</h2>
-        <div><PostAuthor userId={post.user}/></div>
+        <div>
+          <PostAuthor userId={post.user}/>
+          <TimeAgo timestamp={post.date}/>
+        </div>
         <p className="post-content">{post.content}</p>
         <Link to={`/editPost/${post.id}`} className="button">Edit Post</Link>
       </article>
--- redux-example-app/src/features/posts/postsSlice.js
+++ redux-example-app/src/features/posts/postsSlice.js
@@ -1,8 +1,21 @@
 import {createSlice, nanoid} from "@reduxjs/toolkit";
+import {sub} from "date-fns";
 
 const initialState = [
-  {id: "1", title: "First Post!", content: "Hello!", user: "1"},
-  {id: "2", title: "Second Post", content: "More text", user: "2"}
+  {
+    id: "1",
+    title: "First Post!",
+    content: "Hello!",
+    user: "1",
+    date: sub(new Date(), {minutes: 10}).toISOString()
+  },
+  {
+    id: "2",
+    title: "Second Post",
+    content: "More text",
+    user: "2",
+    date: sub(new Date(), {minutes: 5}).toISOString()
+  }
 ];
 
 const postSlice = createSlice({
@@ -17,6 +30,7 @@ const postSlice = createSlice({
         return {
           payload: {
             id: nanoid(),
+            date: new Date().toISOString(),
             title,
             content,
             user: userId
