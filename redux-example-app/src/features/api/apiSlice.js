053a94b35b703c340736f6860d5f7763a4f76606
Kazuhiko Okumura <engkokmr@gmail.com>
2021-11-21 07:37:25 +0900

    Redux Essentials Part8. RTK Query Advanced Patterns.
    Reactionボタンを押したときの操作をRTK Queryで行う
    - サーバーにリアクションを送信するようにした

--- redux-example-app/src/features/api/apiSlice.js
+++ redux-example-app/src/features/api/apiSlice.js
@@ -31,6 +31,33 @@ export const apiSlice = createApi({
         body: post
       }),
       invalidatesTags: (result, error, arg) => [{type: "Post", id: arg.id}]
+    }),
+    addReaction: builder.mutation({
+      query: ({postId, reaction}) => ({
+        url: `posts/${postId}/reactions`,
+        method: "POST",
+        body: {reaction}
+      }),
+      /*
+      * Reactionボタンを押した時に対象の記事だけを更新するようにする
+      * こうするとパージ全体が更新されずに済むらしい
+      * またこれを実行する場合はinvalidatesTagsを追加しなくても指定の記事だけが更新される
+      *  */
+      async onQueryStarted({postId, reaction}, {dispatch, queryFulfilled}) {
+        const patchResult = dispatch(
+          apiSlice.util.updateQueryData('getPosts', undefined, draft => {
+            const post = draft.find(post => post.id === postId)
+            if (post) {
+              post.reactions[reaction]++;
+            }
+          })
+        );
+        try {
+          await queryFulfilled;
+        } catch {
+          patchResult.undo();
+        }
+      }
     })
   })
 });
@@ -39,5 +66,6 @@ export const {
   useGetPostsQuery,
   useGetPostQuery,
   useAddNewPostMutation,
-  useEditPostMutation
+  useEditPostMutation,
+  useAddReactionMutation
 } = apiSlice;
--- redux-example-app/src/features/posts/ReactionButtons.js
+++ redux-example-app/src/features/posts/ReactionButtons.js
@@ -1,6 +1,5 @@
 import React from "react";
-import {useDispatch} from "react-redux";
-import {reactionAdded} from "./postsSlice";
+import {useAddReactionMutation} from "../api/apiSlice";
 
 const reactionEmoji = {
   thumbsUp: "👍",
@@ -11,20 +10,17 @@ const reactionEmoji = {
 };
 
 export const ReactionButtons = ({post}) => {
-  const dispatch = useDispatch();
+  const [addReaction] = useAddReactionMutation();
 
-  const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+  const reactionButtons = Object.entries(reactionEmoji).map(([reactionName, emoji]) => {
     return (
       <button
-        key={name}
+        key={reactionName}
         type="button"
         className="muted-button reaction-button"
-        onClick={() => dispatch(reactionAdded({
-          postId: post.id,
-          reaction: name
-        }))}
+        onClick={() => addReaction({postId: post.id, reaction: reactionName})}
       >
-        {emoji} {post.reactions[name]}
+        {emoji} {post.reactions[reactionName]}
       </button>
     );
   });
