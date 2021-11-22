418f1bf2f2874513b070efa9478776135c32bfb5
Kazuhiko Okumura <engkokmr@gmail.com>
2021-11-23 08:56:48 +0900

    Redux Fundamentals Part5. UI and React
    Initial Setup before start using Redux with React.
    
    Reduxを利用する前のTodoList画面を準備.

--- redux-fundamentals-example/src/App.js
+++ redux-fundamentals-example/src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Header from "./features/header/Header";
+import TodoList from "./features/todos/TodoList";
+import Footer from "./features/footer/Footer";
 
 function App() {
   return (
