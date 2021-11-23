054dcd736b850d795da6f4983ed2c5125a4a7f52
Kazuhiko Okumura <engkokmr@gmail.com>
2021-11-23 09:13:07 +0900

    Redux Fundamentals Part5. UI and React
    Redux Stateを利用してTodoを追加する
    - TodoListにStoreからTodos stateを取得する。
    - useSelectorを使うとStateの取得に加えてsubscribeの機能も持つのでstateが更新されると再レンダする
    - Headerにdispatchを追加し、Enterキーが押されるとTodosへの追加アクションを投げる
    - indexにProviderコンポーネント追加し、App.js へReduxのStoreを渡す

--- redux-fundamentals-example/src/features/todos/TodoList.js
+++ redux-fundamentals-example/src/features/todos/TodoList.js
@@ -1,8 +1,11 @@
 import React from "react";
 import TodoListItem from "./TodoListItem";
+import {useSelector} from "react-redux";
+
+const selectTodos = state => state.todos;
 
 const TodoList = () => {
-  const todos = [];
+  const todos = useSelector(selectTodos);
 
   const renderedListItems = todos.map(todo =>
     <TodoListItem key={todo.id} todo={todo}/>
