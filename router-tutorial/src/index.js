8fd233e379f97b934ff2dde5a20c859be7bfe214
Kazuhiko Okumura <engkokmr@gmail.com>
2021-11-24 05:23:19 +0900

    React Router Add Some Routes
    URLに対してレンダリングする方法を決めるRoute Configを設定
    - レンダーするExpensesとInvoiceを作成
    - Index.jsでURLのPathに対するレンダー要素を指定するためのルーティングを行う
    - これでApp.jsのLink先に対するレンダー要素が決定される

--- router-tutorial/src/index.js
+++ router-tutorial/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import './index.css';
 import App from './App';
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
-    <App/>
+    <Routes>
+      <Route path="/" element={<App/>}/>
+      <Route path="expenses" element={<Expenses/>}/>
+      <Route path="invoices" element={<Invoices/>}/>
+    </Routes>
   </BrowserRouter>,
   rootElement
 );
