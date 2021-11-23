fc56ced8c86bd3339833f2856b10995fac313df2
Kazuhiko Okumura <engkokmr@gmail.com>
2021-11-24 05:10:51 +0900

    React Router Add Some Links
    App.jsにLinkを２つ追加
    - linkをクリックするとURLが変化していることが確認できること
    - styleを追加しているがこのチュートリアルでは大したことではなくここに記載することが重要でもない

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,7 +1,18 @@
+import {Link} from "react-router-dom";
+
 export const App = () => {
   return (
     <dir>
       <h1>Bookkeeper!</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </dir>
   );
 }
